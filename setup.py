4005e29a132a92325b677aa8bd0ed9e9a738d9a7
Ilya Semenov <semenov@inetss.com>
2013-06-27 17:06:10 +0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'django-modelsettings',
+	version = '0.1',
+	packages = ['dbsettings'],
+	license = 'BSD',
+	description = 'Django application that allows to create user-adjustable application settings which are stored in database models',
+	author = 'Ilya Semenov',
+	author_email = 'me@ilyasemenov.com',
+	url = 'https://github.com/IlyaSemenov/django-modelsettings',
+)
