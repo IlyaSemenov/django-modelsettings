76c90d613147651f63aaecaca1c32413ef27b7ce
Ivan Larin <larin@inetss.com>
2013-08-29 15:45:31 +0700

    fix pypi

--- setup.py
+++ setup.py
@@ -7,23 +7,23 @@ which are stored in database models
 """
 
 setup(
-	name = 'django-modelsettings',
-	version = '0.1',
-	url = 'https://github.com/IlyaSemenov/django-modelsettings',
-	license = 'BSD',
-	author = 'Ilya Semenov',
-	author_email = 'me@ilyasemenov.com',
-	description = 'django user-adjastable settings',
-	long_description = __doc__,
-	packages = ['dbsettings'],
-	include_package_data = True,
-	zip_safe = False,
-	platforms = 'any',
-	install_requires = ['dbsettings'],
-	classifiers = [
+	name='django-modelsettings',
+	version='0.1',
+	url='https://github.com/IlyaSemenov/django-modelsettings',
+	license='BSD',
+	author='Ilya Semenov',
+	author_email='me@ilyasemenov.com',
+	description='django user-adjastable settings',
+	long_description=__doc__,
+	packages=['django_modelsettings'],
+	include_package_data=True,
+	zip_safe=False,
+	platforms='any',
+	install_requires=[],
+	classifiers=[
 		# As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 		'Framework :: Django',
-		'Development Status :: 4 - Beta',
+		'Development Status :: 5 - Production/Stable',
 		'Intended Audience :: Developers',
 		'Intended Audience :: Information Technology',
 		'License :: OSI Approved :: BSD License',
