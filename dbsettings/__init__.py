edba9eb2ac43bea18f179e6c80cc775a19112106
Ilya Semenov <ilya@semenov.co>
2016-08-03 23:55:19 +0700

    Added AppConfig

--- dbsettings/__init__.py
+++ dbsettings/__init__.py
@@ -3,3 +3,5 @@ import django.conf
 from .settings import settings
 
 django.conf.settings.db = settings
+
+default_app_config = 'dbsettings.apps.SettingsConfig'
---
+++ dbsettings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SettingsConfig(AppConfig):
+	name = 'dbsettings'
+	verbose_name = "Settings"
