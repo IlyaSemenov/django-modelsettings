f38e7893c0264208906be64106054739fd2e557a
Ilya Semenov <ilya@semenov.co>
2016-08-05 17:45:22 +0700

    Fixed pypi package missing database migrations

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 """Define Django application settings with Django ORM models and edit them in the admin area."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='django-modelsettings',
-	version='0.3.1',
+	version='0.3.2',
 	url='https://github.com/IlyaSemenov/django-modelsettings',
 	license='BSD',
 	author='Ilya Semenov',
 	author_email='ilya@semenov.co',
 	description=__doc__,
 	long_description=open('README.rst').read(),
-	packages=['dbsettings'],
-	package_data={"dbsettings": ["templates/dbsettings/*.html"]},
+	packages=find_packages(),
 	install_requires=['Django>=1.7'],
 	classifiers=[],
 )
