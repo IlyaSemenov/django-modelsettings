a082a24a055ac96cfa77474c63e1aafeaa626108
Ilya Semenov <ilya@semenov.co>
2016-11-21 16:25:30 +0700

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='django-modelsettings',
-	version='0.4.0',
+	version='0.5.0',
 	url='https://github.com/IlyaSemenov/django-modelsettings',
 	license='BSD',
 	author='Ilya Semenov',
