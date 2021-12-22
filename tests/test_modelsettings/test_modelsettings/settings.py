a237b923936d47af6f7f37bfd172ae7c8622265f
Christian González <christian.gonzalez@nerdocs.at>
2021-12-22 20:58:03 +0100

    make it more evident that SECRET_KEY is fake.

--- tests/test_modelsettings/test_modelsettings/settings.py
+++ tests/test_modelsettings/test_modelsettings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+5$j#1%zl*dj*c*v*h75&xz&7$sdwu8ari8w3ee9nawuwny^^#"
+SECRET_KEY = "fake-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
