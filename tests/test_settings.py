bd894743e33ec894ad5e8a16af0bf942238b8f95
Christian González <christian.gonzalez@nerdocs.at>
2022-01-12 10:49:42 +0100

    use native pytest tests instead of Django test suite

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,13 +1,18 @@
+import pytest
 from django.test import TestCase
 from dbsettings import settings
 
 
-class SettingsTest(TestCase):
-    def test_simple(self):
-        self.assertEqual(settings.test1.test_char, "foo")
+@pytest.mark.django_db
+def test_simple():
+    assert settings.test1.test_char == "foo"
 
-    def test_bool_true(self):
-        self.assertEqual(settings.test1.test_bool_true, True)
 
-    def test_bool_false(self):
-        self.assertEqual(settings.test1.test_bool_false, False)
+@pytest.mark.django_db
+def test_bool_true():
+    assert settings.test1.test_bool_true == True
+
+
+@pytest.mark.django_db
+def test_bool_false():
+    assert settings.test1.test_bool_false == False
